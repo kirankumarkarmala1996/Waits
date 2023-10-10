fd4a38badd5a1483dc9c721c21566cffd1b3f257
kirankumarkarmala1996 <108362315+kirankumarkarmala1996@users.noreply.github.com>
2023-10-10 15:01:57 +0530

    add waits
    add waits

--- src/main/java/com/synchronisization/Waits/Waits.java
+++ src/main/java/com/synchronisization/Waits/Waits.java
@@ -1,6 +1,7 @@
 package com.synchronisization.Waits;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.StaleElementReferenceException;
@@ -13,12 +14,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Waits {
 	static WebDriver driver;
+	static WebDriverWait wdwait;
+	static Wait<WebDriver> waits;
 
 	public static void main(String[] args) {
 	}
 
 	static void fluentwait(WebElement element, int time, int pollingTime) {
-
 		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(time))
 				.pollingEvery(Duration.ofSeconds(pollingTime)).ignoring(StaleElementReferenceException.class);
 	}
@@ -46,8 +48,36 @@ public class Waits {
 
 	static void clickOn(By locater, int seconds) {
 		WebDriverWait wait = new WebDriverWait(driver, seconds);
-
 		wait.until(ExpectedConditions.elementToBeClickable(locater)).click();
 
 	}
+	
+	static void fluentwaitToIgnorestalelement(List<WebElement> element, int time, int polligtime) {
+
+		waits = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(time))
+				.pollingEvery(Duration.ofSeconds(polligtime)).ignoring(StaleElementReferenceException.class);
+	}
+
+	static void fluentwaitToIgnorestalelement(By locater, int time, int polligtime) {
+
+		waits = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(time))
+				.pollingEvery(Duration.ofSeconds(polligtime)).ignoring(StaleElementReferenceException.class);
+	}
+
+//	static void waitForElementToPresent(By locater, int time) {
+//		wdwait = new WebDriverWait(driver, time);
+//		wdwait.until(ExpectedConditions.presenceOfElementLocated(locater));
+//	}
+
+	static WebElement waitForElementToVisible(By locater, int time) {
+		wdwait = new WebDriverWait(driver, 10);
+		return wdwait.until(ExpectedConditions.visibilityOfElementLocated(locater));
+	}
+	// explicit wait in selenium ---for particular --element
+
+//	static void elementToVisible(WebDriver driver, WebElement element, int time) {
+//		new WebDriverWait(driver, time).until(ExpectedConditions.visibilityOf(element));
+//	}
+
+
 }
